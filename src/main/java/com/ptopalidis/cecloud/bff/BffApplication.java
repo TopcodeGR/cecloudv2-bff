25c8e578f3376be865e6878feb9e9fea356515ba
TopcodeGR <topcodegr@gmail.com>
2025-05-02 09:34:31 +0300

    asd

--- src/main/java/com/ptopalidis/cecloud/bff/BffApplication.java
+++ src/main/java/com/ptopalidis/cecloud/bff/BffApplication.java
@@ -2,10 +2,18 @@ package com.ptopalidis.cecloud.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
+		"com.topcode.gateway",
+})
+@EnableJpaRepositories(basePackages = {
+		"com.topcode.gateway",
+})
+@EntityScan(basePackages = {
 		"com.topcode.gateway",
 })
 public class BffApplication {
--- src/main/java/com/ptopalidis/cecloud/bff/domain/KeycloakTokenIntrospectionResponse.java
+++
@@ -1,9 +0,0 @@
-package com.ptopalidis.cecloud.bff.domain;
-
-import lombok.Data;
-
-@Data
-public class KeycloakTokenIntrospectionResponse {
-
-    private Boolean active;
-}
